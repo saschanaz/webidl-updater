675d6c2a48e5541b0b1834db280f5d06c0b2cd23
Kagami Sascha Rosylight <saschanaz@outlook.com>
2020-11-02 15:07:15 +0100

    feat(spec-sources): use test-spec when env.WEBIDL_UPDATER_TEST exists
    Closes #10

--- src/spec-sources.js
+++ src/spec-sources.js
@@ -1,4 +1,19 @@
 const browserSources = require("../spec-sources.browsers.generated.json");
 const manualSources = require("../spec-sources.manual.json");
 
-module.exports = { ...browserSources, ...manualSources };
+if (process.env.WEBIDL_UPDATER_TEST) {
+  module.exports = {
+    "https://raw.githubusercontent.com/saschanaz/test-spec/master/index.html": {
+      "shortName": "test-spec",
+      "url": "https://raw.githubusercontent.com/saschanaz/test-spec/master/index.html",
+      "source": "https://github.com/saschanaz/test-spec/blob/HEAD/index.html",
+      "github": {
+        "owner": "saschanaz",
+        "repo": "test-spec",
+        "path": "index.html"
+      }
+    }
+  }
+} else {
+  module.exports = { ...browserSources, ...manualSources };
+}
